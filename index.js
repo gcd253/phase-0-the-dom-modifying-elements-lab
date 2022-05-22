4432bc9b414fd68faf1454a497fa4d15b3c6861c
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-22 15:26:41 -0400

    commit tree 064d06f16a12ba5d41643fc9471493cf8a81c60e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const remover = document.getElementById('main')
+
+remover.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.textContent = "Griffin is the champion"
